73be3e92f37db60a64d7732aaad773119fab4379
Ilya Beliaev <info@php-dev.info>
2025-09-13 10:57:05 +0200

    .

--- src/Widgets/LockboxStatusWidget.php
+++ src/Widgets/LockboxStatusWidget.php
@@ -7,7 +7,7 @@ namespace N3XT0R\FilamentLockbox\Widgets;
 use Filament\Forms\Components\TextInput;
 use Filament\Notifications\Notification;
 use Filament\Widgets\Widget;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\Validate;
 use N3XT0R\FilamentLockbox\Contracts\HasLockboxKeys;
@@ -16,58 +16,79 @@ class LockboxStatusWidget extends Widget
 {
     protected string $view = 'filament-lockbox::widgets.lockbox-status-widget';
 
-    #[Validate('required|min:12')]
+    #[Validate('required|min:8')]
     public ?string $cryptoPassword = null;
+
+    public bool $supportsLockbox = false;
+
+    public function mount(): void
+    {
+        $user = Auth::user();
+        $this->supportsLockbox = $user instanceof HasLockboxKeys;
+    }
 
     public function getFormSchema(): array
     {
+        if (!$this->supportsLockbox) {
+            return [];
+        }
+
         return [
             TextInput::make('cryptoPassword')
                 ->password()
-                ->label('Crypto Password')
+                ->label(__('filament-lockbox::lockbox.form.crypto_password'))
                 ->revealable()
                 ->required()
-                ->minLength(12),
+                ->minLength(8),
         ];
     }
 
     public function generateKey(): void
     {
-        $user = Auth::user();
-        if (!$user instanceof HasLockboxKeys) {
-            return;
-        }
-        $user->initializeUserKeyIfMissing();
-
-        Notification::make()
-            ->title('Lockbox key generated')
-            ->success()
-            ->send();
-
-        $this->dispatch('$refresh');
-
-    }
-
-    public function savePassword(): void
-    {
-        $this->validate();
-
-        /** @var Model&HasLockboxKeys $user */
-        $user = Auth::user();
-
-        if (!$user instanceof HasLockboxKeys) {
+        if (!$this->supportsLockbox) {
             Notification::make()
-                ->title('User model does not support lockbox keys.')
+                ->title(__('filament-lockbox::lockbox.notifications.not_supported'))
                 ->danger()
                 ->send();
 
             return;
         }
 
+        /**
+         * @var User&HasLockboxKeys $user
+         */
+        $user = Auth::user();
+        $user->initializeUserKeyIfMissing();
+
+        Notification::make()
+            ->title(__('filament-lockbox::lockbox.notifications.key_generated'))
+            ->success()
+            ->send();
+
+        $this->dispatch('$refresh');
+    }
+
+    public function savePassword(): void
+    {
+        if (!$this->supportsLockbox) {
+            Notification::make()
+                ->title(__('filament-lockbox::lockbox.notifications.not_supported'))
+                ->danger()
+                ->send();
+
+            return;
+        }
+
+        $this->validate();
+
+        /**
+         * @var User&HasLockboxKeys $user
+         */
+        $user = Auth::user();
         $user->setCryptoPassword($this->cryptoPassword);
 
         Notification::make()
-            ->title('Crypto password set')
+            ->title(__('filament-lockbox::lockbox.notifications.password_set'))
             ->success()
             ->send();
 

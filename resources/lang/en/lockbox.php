01131062c2c98578684e31a48e7b36d853a64661
Ilya Beliaev <info@php-dev.info>
2025-09-13 10:58:05 +0200

    .

--- resources/lang/en/lockbox.php
+++ resources/lang/en/lockbox.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 // translations for N3XT0R/FilamentLockbox
 return [
     'status' => [
-        'not_supported' => '⚠️ This user model does not support Lockbox keys.',
-        'initialized' => '✅ Your Lockbox is initialized.',
-        'missing' => '❌ No Lockbox key found for your account.',
+        'not_supported' => 'This user model does not support Lockbox keys.',
+        'initialized' => 'Your Lockbox is initialized.',
+        'missing' => 'No Lockbox key found for your account.',
     ],
     'buttons' => [
         'generate_key' => 'Generate Lockbox Key',

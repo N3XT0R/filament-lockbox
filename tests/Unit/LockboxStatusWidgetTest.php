f4136b379a2456b699e60f7aa4b6ea04f03db976
Ilya Beliaev <info@php-dev.info>
2025-09-14 23:30:01 +0200

    fix.

--- tests/Unit/LockboxStatusWidgetTest.php
+++ tests/Unit/LockboxStatusWidgetTest.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 namespace N3XT0R\FilamentLockbox\Tests\Unit\Widgets;
 
 use Illuminate\Foundation\Auth\User;
+use Livewire\Livewire;
 use N3XT0R\FilamentLockbox\Contracts\HasLockboxKeys;
 use N3XT0R\FilamentLockbox\Support\KeyMaterial\CryptoPasswordKeyMaterialProvider;
 use N3XT0R\FilamentLockbox\Tests\TestCase;
 use N3XT0R\FilamentLockbox\Widgets\LockboxStatusWidget;
-use Livewire\Livewire;
 
 class LockboxStatusWidgetTest extends TestCase
 {
@@ -25,15 +25,45 @@ class LockboxStatusWidgetTest extends TestCase
     public function testMountWithSupportingUserSetsProvider(): void
     {
         $user = new class () extends User implements HasLockboxKeys {
-            public function getEncryptedUserKey(): ?string { return null; }
-            public function setEncryptedUserKey(string $value): void {}
-            public function getCryptoPasswordHash(): ?string { return null; }
-            public function setCryptoPasswordHash(string $hash): void {}
-            public function getLockboxProvider(): ?string { return CryptoPasswordKeyMaterialProvider::class; }
-            public function setLockboxProvider(string $provider): void {}
-            public function setCryptoPassword(string $plainPassword): void {}
-            public function initializeUserKeyIfMissing(): void {}
-            public function hasLockboxKey(): bool { return false; }
+            public function getEncryptedUserKey(): ?string
+            {
+                return null;
+            }
+
+            public function setEncryptedUserKey(string $value): void
+            {
+            }
+
+            public function getCryptoPasswordHash(): ?string
+            {
+                return null;
+            }
+
+            public function setCryptoPasswordHash(string $hash): void
+            {
+            }
+
+            public function getLockboxProvider(): ?string
+            {
+                return CryptoPasswordKeyMaterialProvider::class;
+            }
+
+            public function setLockboxProvider(string $provider): void
+            {
+            }
+
+            public function setCryptoPassword(string $plainPassword): void
+            {
+            }
+
+            public function initializeUserKeyIfMissing(): void
+            {
+            }
+
+            public function hasLockboxKey(): bool
+            {
+                return false;
+            }
         };
 
         Livewire::actingAs($user)->test(LockboxStatusWidget::class)

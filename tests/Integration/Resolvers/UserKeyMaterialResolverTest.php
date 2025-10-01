4eb75397d478646f2bc86a40d849cae5a8a2d688
Ilya Beliaev <info@php-dev.info>
2025-10-01 20:46:07 +0200

    test(resolver): add coverage for specific provider resolution paths

--- tests/Integration/Resolvers/UserKeyMaterialResolverTest.php
+++ tests/Integration/Resolvers/UserKeyMaterialResolverTest.php
@@ -6,6 +6,7 @@ namespace N3XT0R\FilamentLockbox\Tests\Integration\Resolvers;
 
 use Filament\Auth\MultiFactor\App\AppAuthentication;
 use Illuminate\Foundation\Auth\User as BaseUser;
+use N3XT0R\FilamentLockbox\Contracts\UserKeyMaterialProviderInterface;
 use N3XT0R\FilamentLockbox\Managers\KeyMaterial\TotpKeyMaterialProvider;
 use N3XT0R\FilamentLockbox\Resolvers\UserKeyMaterialResolver;
 use N3XT0R\FilamentLockbox\Tests\Stubs\Auth\TotpUser;
@@ -86,5 +87,67 @@ class UserKeyMaterialResolverTest extends TestCase
         $this->expectExceptionMessageMatches('/No UserKeyMaterial provider could handle model/');
 
         $resolver->resolve($user, '123456');
+    }
+
+    public function testResolveWithSpecificProviderSucceeds(): void
+    {
+        $user = new TotpUser();
+        $user->id = 1;
+
+        $this->mock(AppAuthentication::class)
+            ->shouldReceive('verifyCode')
+            ->once()
+            ->with('123456', $user->getAppAuthenticationSecret())
+            ->andReturn(true);
+
+        $resolver = app(UserKeyMaterialResolver::class);
+        $resolver->registerProvider(new TotpKeyMaterialProvider());
+
+        $material = $resolver->resolve($user, '123456', TotpKeyMaterialProvider::class);
+
+        $this->assertSame(
+            hash('sha256', $user->getAppAuthenticationSecret() . $user->getKey(), true),
+            $material,
+        );
+    }
+
+    public function testResolveWithSpecificProviderThrowsIfNotSupporting(): void
+    {
+        $user = new TotpUser();
+        $user->id = 1;
+
+        // Dummy provider that never supports
+        $dummyProvider = new class () implements UserKeyMaterialProviderInterface {
+            public function supports($user): bool
+            {
+                return false;
+            }
+
+            public function provide($user, ?string $input): string
+            {
+                return 'dummy';
+            }
+        };
+
+        $resolver = app(UserKeyMaterialResolver::class);
+        $resolver->registerProvider($dummyProvider);
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessageMatches('/does not support model/');
+
+        $resolver->resolve($user, 'anything', $dummyProvider::class);
+    }
+
+    public function testResolveWithSpecificProviderThrowsIfNotRegistered(): void
+    {
+        $user = new TotpUser();
+        $user->id = 1;
+
+        $resolver = app(UserKeyMaterialResolver::class);
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessageMatches('/is not registered/');
+
+        $resolver->resolve($user, 'whatever', 'Unregistered\\Provider\\Class');
     }
 }
